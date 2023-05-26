a70b7c3597d6e5422d9befdad82dc619182e1bf4
Sávio Batista <saviobatista@email.com>
2023-05-26 19:02:51 +0000

    fix: cron over network names not ip

--- cron.sh
+++ cron.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 PATH=$PATH":/home/savio/.nvm/versions/node/v18.12.1/bin"
 echo $PATH
-scp takp@10.42.17.50:/home/takp/log/adsb_log.$(date --date="yesterday" +%Y%m%d).csv.gz /home/savio/adsb-data/
+scp takp@S-KPAN50:/home/takp/log/adsb_log.$(date --date="yesterday" +%Y%m%d).csv.gz /home/savio/adsb-data/
 cd /home/savio/atm
 yarn workspace adsb run start parse SBKP --pattern $(date --date="yesterday" +%Y%m%d).*
-
